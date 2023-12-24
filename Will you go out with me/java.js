53744d1a6d6acd6702f123cf3e206d073f3e1deb
ValchoNOOB <96371784+ValchoNOOB@users.noreply.github.com>
2023-12-24 12:07:38 +0200

    Delete Will you go out with me directory

--- Will you go out with me/java.js
+++
@@ -1,9 +0,0 @@
-const btnNo = document.getElementById('btnNo');
-
-btnNo.addEventListener('mouseover', () => {
-  const newX = Math.random() * window.innerWidth; // Generate a random X-coordinate
-  const newY = Math.random() * window.innerHeight; // Generate a random Y-coordinate
-
-  btnNo.style.left = `${newX}px`; // Set the new X-coordinate
-  btnNo.style.top = `${newY}px`; // Set the new Y-coordinate
-});
